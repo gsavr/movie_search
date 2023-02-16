0cc2b0903ac03350bb22fa64938f26a9b67a5a7e
giorgio <giorgiosavron@me.com>
2023-02-15 19:04:55 -0500

    fixed framer-motion

--- src/components/sticky-menu.tsx
+++ src/components/sticky-menu.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-scroll";
 
 export const StickyMenu: React.FC = () => {
---
+++ src/framer-motion.d.ts
@@ -0,0 +1,3 @@
+declare module "../node_modules/framer-motion/dist/framer-motion" {
+  export * from "framer-motion";
+}
